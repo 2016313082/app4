8f0541c3931308c96288ac7ec7203f49ebb29f8a
gustavo <gustavo.vtamayo@outlook.com>
2021-10-05 21:39:54 -0500

    Cambio de páginas

---
+++ app4/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using app4.ViewModels;
+using Xamarin.Forms;
+
+namespace app4
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainPageViewModel(navigation: Navigation);
+        }
+    }
+}
